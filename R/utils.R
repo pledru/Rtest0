8fd07944eb10cbb5bdc988b19cddb4c26e8f2776
Pascal Ledru <pascal.ledru@oracle.com>
2022-09-27 16:08:50 -0600

    more

--- R/utils.R
+++ R/utils.R
@@ -7,6 +7,10 @@ test_it <- function() {
   print("test it...")
 }
 
+#' A sample function
+#'
+#' This function does not take any input and return a sample message.
+#'
 test_it2 <- function() {
     r = "test 0"
     r
