0f67edcda643abba3da679fc484ecf3c31c344c4
Pascal Ledru <pascal.ledru@oracle.com>
2022-09-27 17:34:52 -0600

    more

--- R/utils.R
+++ R/utils.R
@@ -1,23 +1,9 @@
-#' A sample function
-#'
-#' This function does not take any input and return a sample message.
-#'
 #' @export
-#
-#' @examples
-#' test_it()
 test_it <- function() {
   print("test it...")
 }
 
-#' A sample function
-#'
-#' This function does not take any input and return a sample message.
-#'
 #' @export
-#
-#' @examples
-#' test_it()
 test_it2 <- function() {
     r = "test 0"
     r
