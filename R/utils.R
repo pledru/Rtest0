685f10b5463208bb04559eddd098cd7ac39f78ae
Pascal Ledru <pascal.ledru@oracle.com>
2022-09-28 09:06:32 -0600

    more

--- R/utils.R
+++ R/utils.R
@@ -3,6 +3,10 @@ test_it <- function() {
   print("test it...")
 }
 
+#' Example hoe to call ggplot
+#
+#' @import tidyverse
+#
 #' @export
 test_plot <- function() {
   ggplot(data = mpg) + geom_point(mapping = aes(x = displ, y = hwy))
